f8ba58991a40b3afd1cb6448eb0fde9b56540c16
python357-1 <jb2101554@gmail.com>
2022-10-20 03:03:15 -0500

    final commit

--- algorithms/caeser.nim
+++ algorithms/caeser.nim
@@ -3,7 +3,7 @@ import sequtils, strutils
 proc caeserCipher*(input: string, shift: int): string =
     var alphabet: array[26, char] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
-    # i know this is some bullshit. thats why i did it
+    # now 100% bullshit free!
     var numarray = map(cast[seq[char]](toLowerAscii(strip(input))), proc(x: char): int = (if x == ' ': -5 else: find(alphabet, x) + shift))
     var decodedarray = map(numarray, proc(y: int): char = (if y < 0: ' ' else: alphabet[(if y >= 26: y-26 else: y)]))
     var ciphered = join(decodedarray)
--- main.nim
+++ main.nim
@@ -1,6 +1,5 @@
-import strutils
+import strutils, strformat
 import std/random
-
 import algorithms/caeser
 
 randomize()
@@ -15,6 +14,39 @@ var numLines: int = len(line)
 numLines = numLines - 1
 var plainText = line[rand(numLines)]
 
-var cipherKey = rand(24)
-echo caeserCipher($plainText, cipher_key)
-echo "Cipher Key: " & $cipher_key
+var cipherKey = rand(23) + 1
+var res = caeserCipher($plainText, cipherKey)
+var workingSolution: seq[char] = cast[seq[char]](res)
+
+var placeholders: seq[string] = @[]
+
+for letter in workingSolution:
+    if (find(placeholders, cast[seq[char]](letter)) != -1):
+        placeholders.add(cast[string](letter))
+
+var numberedSolution: seq[string] = cast[seq[string]](workingSolution)
+for lett in placeholders:
+    var y = 1
+    while (find(numberedSolution, lett) != -1):
+        numberedSolution[find(numberedSolution, lett)] = $y
+    y += 1
+
+
+echo join(workingSolution)
+while workingSolution != plainText:
+    var input: string = readLine(stdin); # user input should be in form of xx:y or xx=y or xx y
+    case (join(input)):
+        of "solution":
+            echo plainText
+        of "shift":
+            echo cipherKey 
+        of "reset":
+            workingSolution = cast[seq[char]](res)
+        else:
+            var inputFmt = align(input, 4, '0')
+            var replaced = parseInt(fmt"{inputFmt[0]}{inputFmt[1]}") - 1
+            var replacedWith = inputFmt[3]
+            workingSolution[replaced] = replacedWith
+    echo join(workingSolution)
+    echo align("", len(workingSolution), '-')
+    #echo join(numberedSolution)
